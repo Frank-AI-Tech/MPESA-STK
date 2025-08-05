0ddd3d7730e7f9f901d331fd6a82763d94d1000b
Confronter <confronterbackup@gmail.com>
2025-08-05 13:55:54 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 
 
 const config = {
-    consumerKey: process.env.CONSUMER_KEY || "",
-    consumerSecret: process.env.CONSUMER_SECRET || "",
+    consumerKey: process.env.CONSUMER_KEY || "TEyb2o1QieawGAwxyQQkz57qfBJ7wiLOEuN72UDhPfC0JEkg",
+    consumerSecret: process.env.CONSUMER_SECRET || "PDmvKcPIm83M3f0swhNgXU0lWhek9EAlWhPbNH9sMFATkwTu6ktZiXwfUATL9xA3",
     shortCode: process.env.SHORTCODE || "174379",
     passkey: process.env.PASSKEY || "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919",
     number: process.env.PUSHNUMBER || "254708374149",
